10c4414b2a9d4b0c8add14c1a6129480ec9623d7
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-12-03 09:27:52 +0100

    created install shell script

---
+++ liberty/install-locale.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -r "./wlp"
+
+mvn -f ../pom.xml clean net.wasdev.wlp.maven.plugins:liberty-maven-plugin:2.5:install-server
+
+mv "../target/liberty/wlp" "./wlp"
+
+"wlp/bin/server" create "openliberty-microprofile-example"
+
+cp -f "./config/server-locale.xml" "wlp/usr/servers/openliberty-microprofile-example/server.xml"
+
+"wlp/bin/installUtility" install microprofile-2.0
