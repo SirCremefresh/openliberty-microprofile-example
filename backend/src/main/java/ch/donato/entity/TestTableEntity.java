0fdb50f6d577661b9639fb8ca1a5a8ba5aa3905a
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-12-07 10:11:36 +0100

    fixed auto generated id

--- backend/src/main/java/ch/donato/entity/TestTableEntity.java
+++ backend/src/main/java/ch/donato/entity/TestTableEntity.java
@@ -1,9 +1,6 @@
 package ch.donato.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "test_table")
@@ -12,7 +9,6 @@ public class TestTableEntity {
 	public String name;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public int id;
-
-
 }
